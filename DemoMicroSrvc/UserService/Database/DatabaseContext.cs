816a2e0c771b5ed85070ed0f44c6b9efbbbb0796
Rohith <varma36a@gmail.com>
2023-04-17 16:39:06 +0530

    connection string

---
+++ DemoMicroSrvc/UserService/Database/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService.Database.Entities;
+
+namespace UserService.Database
+{
+    public class DatabaseContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=ROHIT36A\SQLEXPRESS; Initial Catalog=UserMicroservice;UID=project1; PWD=Varma@123;");
+        }
+    }
+}
